efd54df49e2f7f64ace6ff76a272dd2cceb5eab6
DevAnas17 <anasdeveloper17@gmail.com>
2023-01-18 20:24:21 +0530

    add deploy scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy"
+import "@nomiclabs/hardhat-ethers"
+
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -10,7 +13,20 @@ const config: HardhatUserConfig = {
         runs: 100
       }
     }
-  }
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // INDEX for the deployer's account in the array of accounts that Hardhat gives us.
+    },
+  },
 };
 
 export default config;
